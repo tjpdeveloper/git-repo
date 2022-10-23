86317c8110539a30d31681bb54bf07d0ae9070a9
Prakash <tjpdeveloper8@gmail.com>
2022-10-23 09:06:00 +0200

    Add new sop statment

--- src/main/java/com/javatechie/app/gitdemo/App.java
+++ src/main/java/com/javatechie/app/gitdemo/App.java
@@ -3,6 +3,7 @@ package com.javatechie.app.gitdemo;
 public class App {
 
 	public static void main(String[] args) {
+		System.out.println("Hello Java World");
 		System.out.println("Hello Git World");
 	}
 
