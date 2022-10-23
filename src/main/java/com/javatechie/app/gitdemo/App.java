bdaa8cf51e84a1e3d865e5ce781fd7c5d7716d73
tjpdeveloper <95494910+tjpdeveloper@users.noreply.github.com>
2022-10-23 09:55:19 +0200

    Added new sop 3rd in github

--- src/main/java/com/javatechie/app/gitdemo/App.java
+++ src/main/java/com/javatechie/app/gitdemo/App.java
@@ -5,6 +5,7 @@ public class App {
 	public static void main(String[] args) {
 		System.out.println("Hello Java World");
 		System.out.println("Hello Git World");
+		System.out.println("Add new chaneges in Github directly");
 	}
 
 }
